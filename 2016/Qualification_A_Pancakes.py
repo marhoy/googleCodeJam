ef02aa04c621b4853856f44340baf09c23415485
marhoy <martin.hoy@pvv.ntnu.no>
2017-03-24 22:12:15 +0100

    Initial commits

--- 2016/Qualification_A_Pancakes.py
+++ 2016/Qualification_A_Pancakes.py
@@ -1,14 +1,32 @@
 
 
-# input() reads a string with a line of input, stripping the '\n' (newline) at the end.
-# This is all you need for most Google Code Jam problems.
+def minimum_flips(pancakes):
+    grouped_height = 1 + pancakes.count('-+') + pancakes.count('+-')
+    if pancakes.endswith('-'):
+        return grouped_height
+    else:
+        return grouped_height - 1
+
+
+def flip(string):
+    l = ['-' if c == '+' else '+' for c in string]
+    return ''.join(l)
+
+
 num_cases = int(input())
 
 for case in range(1, num_cases + 1):
     string = input()
-    stack = [0 if c == '-' else 1 for c in string]
+    orig_string = string
 
-    grouped_height = 0
-    for i in len(stack):
+    flips = 0
+    while True:
+        i = string.rfind('-')
+        if i < 0:
+            break
+        # print('\tFlipping at index', i)
+        string = flip(string[:i+1]) + string[i+1:]
+        flips += 1
+        # print('\t', string)
 
-    print("Case #{}: {}".format(case, stack))
+    print("Case #{}: {}".format(case, flips))
