5ad98addc0661c7146848526af1d922e46265217
marhoy <martin.hoy@pvv.ntnu.no>
2017-03-28 12:53:37 +0200

    Initial commits

--- 2016/Qualification_B_Pancakes.py
+++ 2016/Qualification_B_Pancakes.py
@@ -1,5 +1,11 @@
+import logging
+
+# Package-wide logging configuration
+logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
+LOG = logging.getLogger()
 
 
+# Solution from Google...
 def minimum_flips(pancakes):
     grouped_height = 1 + pancakes.count('-+') + pancakes.count('+-')
     if pancakes.endswith('-'):
@@ -10,7 +16,7 @@ def minimum_flips(pancakes):
 
 def flip(string):
     l = ['-' if c == '+' else '+' for c in string]
-    return ''.join(l)
+    return ''.join(reversed(l))
 
 
 num_cases = int(input())
@@ -18,15 +24,24 @@ num_cases = int(input())
 for case in range(1, num_cases + 1):
     string = input()
     orig_string = string
+    LOG.info('Problem: %s', string)
 
     flips = 0
     while True:
         i = string.rfind('-')
         if i < 0:
             break
-        # print('\tFlipping at index', i)
-        string = flip(string[:i+1]) + string[i+1:]
+
+        part1 = string[:i+1]
+        part2 = string[i+1:]
+        LOG.debug('Part 1: %s, Part 2: %s', part1, part2)
+        if part1.startswith('+'):
+            i = part1.find('-')
+            part1 = flip(part1[:i]) + part1[i:]
+            flips += 1
+            LOG.debug('New part1: %s', part1)
+        string = flip(part1) + part2
         flips += 1
-        # print('\t', string)
+        LOG.info('After %d flips: %s', flips, string)
 
     print("Case #{}: {}".format(case, flips))
