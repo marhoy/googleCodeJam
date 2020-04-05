a481c7eaf3ad7cceb34eeda34dc96cd4b46de4bf
Martin Høy <martin.hoy@dnvgl.com>
2020-04-05 18:22:15 +0000

    Use fileinput

--- codejam_skeleton.py
+++ codejam_skeleton.py
@@ -1,52 +1,60 @@
 import argparse
+import fileinput
 import functools
 import logging
-import sys
+from typing import List
 
 # Redefine the print function with flush=True. Needed for interactive problems.
 print = functools.partial(print, flush=True)
 
 # Configure logging
-logging.basicConfig(format='[%(lineno)03d]: %(message)s', level=logging.WARNING)
+logging.basicConfig(
+    format='[%(lineno)03d]: %(message)s', level=logging.WARNING)
 LOG = logging.getLogger(__name__)
 
 
-def get_int():
+def get_int() -> int:
+    """Read an int from file or stdin."""
     string = FILE.readline().strip()
     return int(string)
 
 
-def get_float():
+def get_float() -> float:
+    """Read a float from file or stdin."""
     string = FILE.readline().strip()
     return float(string)
 
 
-def get_ints():
+def get_ints() -> List[int]:
+    """Read multiple ints from file or stdin."""
     string = FILE.readline().strip()
     return [int(s) for s in string.split()]
 
 
-def get_string():
+def get_string() -> str:
+    """Read a string from file or stdin."""
     string = FILE.readline().strip()
     return string
 
 
 def main():
+    """This is where you write your solution."""
     cases = get_int()
 
     for case in range(1, cases + 1):
         # Your solution goes here
-
-        print('Case #{}:'.format(case))
+        a = get_int()
+        print('Case #{}: {}'.format(case, a))
 
 
 if __name__ == '__main__':
     # Parse command line arguments
     parser = argparse.ArgumentParser(description='Code Jam solution')
     parser.add_argument('-v', '--verbose', action='count', default=0,
-                        help="Increase verbosity. Can be repeated up to two times.")
-    parser.add_argument('infile', nargs='?', type=argparse.FileType('r'),
-                        default=sys.stdin, help="Read from file instead of stdin")
+                        help="Increase verbosity. "
+                             "Can be repeated up to two times.")
+    parser.add_argument('infile', default="-", nargs="?",
+                        help="Read from file instead of stdin")
     arguments = parser.parse_args()
 
     # Possibly change logging level of the top-level logger
@@ -59,5 +67,5 @@ if __name__ == '__main__':
     LOG.debug("Finished parsing arguments: %s", arguments)
 
     # Define a global FILE variable (sys.stdin or infile) and run main()
-    FILE = arguments.infile
+    FILE = fileinput.input(files=arguments.infile)
     main()
